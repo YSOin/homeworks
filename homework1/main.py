20c83e16f52c3064fb4c3e7e438bddcae8552ba3
Serhii Yavorskiy <oin@Serhiis-MacBook-Pro.local>
2023-05-29 23:09:23 +0300

    revorked realization cache class, and added function decorator with the same cache logic

--- homework1/main.py
+++ homework1/main.py
@@ -1,22 +1,12 @@
-import pathlib
-import os
-import random
-import time
-import timeit
 import functools
 import requests
-
-import os, psutil
-
-# pid = os.getpid()
-
-
+import os
+import psutil
 
 
 
 class LFUCache:
-    def __init__(self, func, max_limit = 10):
-        self.func = func
+    def __init__(self, max_limit = 10):
         self.cache = dict()
         self.counter = dict()
         self.max_limit = max_limit
@@ -33,24 +23,49 @@ class LFUCache:
     def remove(self):
         self.cache.pop(min(self.counter))
 
-    def __call__(self, *args, **kwargs):
-        cache_key = (args, tuple(kwargs.items()))
-        self.update_counter(cache_key)
-        if cache_key in self.cache:
-                return self.cache[cache_key]
-        result = self.func(*args, **kwargs)
-        if len(self.cache) >= self.max_limit:
-            self.remove()
-            self.add(cache_key, result)
-        else:
-            self.add(cache_key, result)
-        return result
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+            self.update_counter(cache_key)
+            if cache_key in self.cache:
+                    return self.cache[cache_key]
+            result = func(*args, **kwargs)
+            if len(self.cache) >= self.max_limit:
+                self.remove()
+                self.add(cache_key, result)
+            else:
+                self.add(cache_key, result)
+            return result
+        return wrapper
+
+def cache(max_limit=10):
+    def caching_decorator(fn):
+        def decorated_fn(*args ,**kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+            cache = dict()
+            counter = dict()
+            if cache_key in cache:
+                counter[cache_key]+=1
+            else:
+                counter.update({cache_key:1})
+            if cache_key in cache:
+                return cache[cache_key]
+            result = fn(*args, **kwargs)
+            if len(cache) >= max_limit:
+                cache.pop(min(counter))
+                cache.update({cache_key:result})
+            else:
+                cache.update({cache_key:result})
+            return result
+        return decorated_fn
+    return caching_decorator
+
+
 
 
 def memory_usage(f):
     @functools.wraps(f)
     def deco(*args, **kwargs):
-        start = time.time()
         result = f(*args, **kwargs)
         memory_used = psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2
         print('Memory_used', f'({f.__name__}): {memory_used}MiB')
@@ -60,13 +75,12 @@ def memory_usage(f):
 
 
 
-
-@LFUCache
+# @LFUCache(max_limit=13)
+@cache(max_limit=12)
 @memory_usage
 def some_function(url, first_n=100):
     """Fetch a given url"""
     res = requests.get(url)
-    print(url)
     return res.content[:first_n] if first_n else res.content
 
 some_function('https://google.com')
